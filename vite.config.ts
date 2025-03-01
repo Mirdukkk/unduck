7e9655ce9bccfbdc354f92967c075759bd9d55a3
Mirdukkk <44965055+Mirdukkk@users.noreply.github.com>
2025-03-01 18:27:07 +0500

    fix: allow to cache large files

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ export default defineConfig({
   plugins: [
     VitePWA({
       registerType: "autoUpdate",
+      workbox: {
+        // 5 MB
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+      },
     }),
   ],
 });
